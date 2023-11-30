39188c7c87dad1e140333975a4bc04edd9953baa
Haley Vlahos <horseslovehaley@gmail.com>
2023-11-29 18:31:41 -0800

    Boss Navmesh 2.0

--- Assets/_Scripts/AI Scripts/Boss_Navigation.cs
+++ Assets/_Scripts/AI Scripts/Boss_Navigation.cs
@@ -9,13 +9,15 @@ public class Boss_Navigation : MonoBehaviour
     private int pIndex;
     private NavMeshAgent agent;
     [SerializeField] private bool moving;
+    private bool forward;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         pIndex = 0;
         moving = false;
-        Count_Manager.bossAppears += startMoving;
+        forward = true;
+        Count_Manager.bossAppears += moveForward;
     }
 
     private void Update()
@@ -26,16 +28,28 @@ public class Boss_Navigation : MonoBehaviour
                     if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f) {
                         if (pIndex >= 0 && pIndex < points.Length) {
                             move(points[pIndex].position);
-                            pIndex++;
+
                         } else {
                             moving = false;
-                            pIndex = 0;
+                            //if(forward) {
+                            // START VOTING STUFF HERE
+                            //}
+                            forward = !forward;
                         }
+                        xCrement();
                     }
                 }
             }
         }
 
+    }
+
+    private void xCrement()
+    {
+        if(forward)
+            pIndex++;
+        else
+            pIndex--;
     }
 
 
@@ -44,9 +58,20 @@ public class Boss_Navigation : MonoBehaviour
         agent.SetDestination(loc);
     }
 
-    private void startMoving()
+    private void moveForward()
     {
+        pIndex = 0;
         moving = true;
+        forward = true;
+        gameObject.transform.rotation = points[0].rotation;
+        gameObject.transform.position = points[0].position;
+    }
+
+    public  void moveBackward()
+    {
+        pIndex = points.Length - 1;
+        moving = true;
+        forward = false;
     }
 
 }
